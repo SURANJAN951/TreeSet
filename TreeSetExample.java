f0fbb6bad73db543584187d51b539f8b76c0d31e
Suranjan kumar <ksuranjan951@gmail.com>
2024-08-21 12:05:43 +0530

    TreeSet

---
+++ TreeSetExample.java
@@ -0,0 +1,12 @@
+import java.util.TreeSet;
+
+public class TreeSetExample {
+
+    public static void main(String[] args) {
+        TreeSet ts = new TreeSet();
+        ts.add(100);
+        ts.add(50);
+        ts.add(150);
+        System.out.println(ts);
+    }
+}
